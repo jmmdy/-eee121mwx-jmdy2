5d8e0c084a325672e45c24ab56219a398fd987a3
jmmdy <jmdy2@up.edu.ph>
2024-04-22 22:46:29 +0800

    updated

---
+++ lab07/lab07ex01_jmdy2.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+int main() {
+    int N,M;
+    int u,v;
+
+    vector< vector<int> > adj_list;
+    
+    bool* visited;
+
+    //use undirected graph as input
+    cin >> N >> M;
+    int adj_mat[N][N];
+
+    adj_list.resize(N);
+    visited = new bool[N];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            adj_mat[i][j] = 0;
+        }}
+
+    for(int i = 0; i < M; i++) {
+        cin >> u >> v;
+        adj_mat[u][v] = 1;
+        // adj_mat[v][u] = 1;
+    }
+
+    for(int i = 0; i < N; i++) {
+        visited[i] = false;
+    }
+
+    int userinput;
+    cin >> userinput;
+    if (userinput == 1){
+        queue<int> bfs_queue;   
+    
+        bfs_queue.push(0);
+        
+        visited[0] = true;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << adj_mat[i][j] << " ";
+            }
+            cout << endl;
+            }
+        while(!bfs_queue.empty()) {
+            int u = bfs_queue.front();
+            // int u = bfs_queue.top();
+            bfs_queue.pop();
+            cout << char(u + 'A') << " ";
+            for(size_t i = 0; i < N; i++) {
+                int v = i;
+                if(!visited[v]) {
+                    visited[v] = true;
+                    bfs_queue.push(i);
+                }
+            }
+        }
+
+        cout << endl;
+
+        delete[] visited;
+    }
+        
+        
+    else {
+        stack<int> dfs_stack;
+        dfs_stack.push(0);
+        
+        visited[0] = true;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << adj_mat[i][j] << " ";
+            }
+        cout << endl;
+        }
+        while(!dfs_stack.empty()) {
+            int u = dfs_stack.top();
+            dfs_stack.pop();
+            cout << char(u + 'A') << " ";
+            for(int i = 0; i < N; i++) {
+                int v = i;
+                if(!visited[v]) {
+                    visited[v] = true;
+                    dfs_stack.push(v);
+                }
+            }
+        }
+
+        cout << endl;
+        
+        delete[] visited;
+    }
+
+    return 0;
+}
---
+++ lab07/lab07ex02_jmdy2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+void printAdjList(vector<vector<int>> &adj_list,vector<string> &apparel) {
+    for (int i = 0; i < adj_list.size(); i++) {
+        cout << apparel[i] << "->";
+        for (int v : adj_list[i]) {
+            cout << apparel[v] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+void toposort(int u, vector< vector<int> > &adj_list, bool* visited, queue<int> &topo_stack) {
+    visited[u] = true;
+
+    for(vector<int>::size_type i = 0; i < adj_list[u].size(); i++) {
+        int v = adj_list[u][i];
+        if(!visited[v]) {
+            toposort(v,adj_list,visited,topo_stack);
+        }
+    }
+
+    topo_stack.push(u);
+}
+
+int main() {
+    int N,M;
+    int u,v;
+    vector< vector<int> > adj_list;
+    bool* visited;
+    string* apparel;
+
+    cin >> N >> M;
+    cin.get();
+    
+    adj_list.resize(N);
+    visited = new bool[N];
+    apparel = new string[N];
+
+    for(int i = 0; i < N; i++) {
+        cin >> apparel[i];
+        visited[i] = false;
+    }
+
+    for(int  i =0; i < M; i++) {
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+    }
+
+    queue<int> topo_stack;
+
+    for(int i =0; i < N; i++) {
+        if(!visited[i]) {
+            toposort(i, adj_list, visited, topo_stack);
+        }
+    }
+    printAdjList(adj_list, apparel[topo_stack.front()] );
+
+    while(!topo_stack.empty()) {
+        cout << apparel[topo_stack.front()];
+        topo_stack.pop();
+        if(topo_stack.size()) {
+            cout << " -> ";
+        }
+    }
+    // while(!topo_stack.empty()) {
+    //     cout << apparel[topo_stack.top()];
+    //     topo_stack.pop();
+    //     if(topo_stack.size()) {
+    //         cout << " -> ";
+    //     }
+    // }
+    cout << endl;
+
+    delete[] visited;
+    delete[] apparel;
+
+    return 0;
+}
